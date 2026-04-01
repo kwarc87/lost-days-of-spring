5b88eb68cfb525810cd351aaaa7ed4dfb32fcc64
jakub.kwarcinski <jakubkwarcinski@gmail.com>
2026-04-01 22:16:28 +0200

    Lost Days Of Spring - Platformer game - Cache burst fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { LostDaysOfSpring } from "./core/LostDaysOfSpring.js";
+const mainUrl = new URL(import.meta.url);
+const version = mainUrl.searchParams.get("v");
+const coreModulePath = version
+	? `./core/LostDaysOfSpring.js?v=${version}`
+	: "./core/LostDaysOfSpring.js";
+
+const { LostDaysOfSpring } = await import(coreModulePath);
 
 const game = new LostDaysOfSpring("game", true);
 game.start();
